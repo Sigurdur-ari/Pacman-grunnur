b222ea43e19c73410c09867a1563b72baee5522a
Jonnn358 <166008125+Jonnn358@users.noreply.github.com>
2024-04-08 12:55:27 +0000

    Add files via upload

---
+++ Pacman/src/main/java/vidmot/pacman/Maze.java
@@ -0,0 +1,214 @@
+package vidmot.pacman;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Rectangle;
+
+import java.io.IOException;
+import java.util.Iterator;
+
+public class Maze extends Pane {
+    @FXML
+    private Pacman fxPacman;
+
+    @FXML
+    private Maze fxMaze;
+
+    @FXML
+    private Group veggirGroup;
+
+    @FXML
+    private Group pelletGroup;
+
+    @FXML
+    private Group cherryGroup;
+
+    @FXML
+    private Label fxStig;
+
+    @FXML
+    private Label fxTimi;
+
+    private IntegerProperty Stig = new SimpleIntegerProperty(0);
+
+    private IntegerProperty Timi = new SimpleIntegerProperty(90);
+
+    /**
+     * Les inn FXML skránna fyrir Maze, til að gera tvö borð væri hægt að útfæra Maze1 og Maze2 t.d.
+     * og hafa þá svipaða skrá sem heldur bara utan um sitt hvort mazeið.
+     */
+    public Maze(){
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("maze-view.fxml"));
+        fxmlLoader.setRoot(this);
+        fxmlLoader.setController(this);
+        try {
+            fxmlLoader.load();
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+        fxStig.textProperty().bind(Stig.asString("Stig: %d"));
+        fxTimi.textProperty().bind(Timi.asString("Tími: %d"));
+    }
+
+    public boolean erAVegg(){
+        for (Node node : veggirGroup.getChildren()) {
+            Rectangle wall = (Rectangle) node;
+            if (fxPacman.getBoundsInParent().intersects(wall.getBoundsInParent())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Setur stefnuna á pacman
+     */
+    public void setStefna(Stefna stefna){
+        fxPacman.setStefna(stefna);
+    }
+
+    public Stefna getFyrriPacmanStefna(){
+        return fxPacman.getFyrriStefna();
+    }
+
+    public int getPacmanLayoutX(){
+        return (int) fxPacman.getLayoutX();
+    }
+    public int getPacmanLayoutY(){
+        return (int) fxPacman.getLayoutY();
+    }
+
+    public void setPacmanLayoutX(int nuvX, int breyt){
+        fxPacman.setLayoutX(nuvX + breyt);
+    }
+    public void setPacmanLayoutY(int nuvY, int breyt){
+        fxPacman.setLayoutY(nuvY + breyt);
+    }
+
+
+
+    /**
+     * Kallar á afram aðferðinni á pacman.
+     */
+    public void afram(){
+        if(!erAVegg()){
+            fxPacman.afram();
+        }
+    }
+
+    public boolean erLoglegStefna(Stefna nuverandiStefna) {
+        double pacmanX = fxPacman.getLayoutX();
+        double pacmanY = fxPacman.getLayoutY();
+
+        int nyttX = (int) pacmanX;
+        int nyttY = (int) pacmanY;
+
+        switch (nuverandiStefna) {
+            case Stefna.UPP:
+                nyttY += 5;
+                break;
+            case Stefna.NIDUR:
+                nyttY -= 5;
+                break;
+            case Stefna.HAEGRI:
+                nyttX -= 5;
+                break;
+            case Stefna.VINSTRI:
+                nyttX += 5;
+                break;
+            default:
+                return false;
+        }
+
+        if (nyttX < 0 || nyttX >= this.getWidth() || nyttY < 0 || nyttY >= this.getHeight()) {
+            return false;
+        }
+        if (ennIVegg(nyttX, nyttY)) {
+            return false;
+        }
+        return true;
+    }
+
+    public boolean ennIVegg(int x, int y){
+        for (Node barn : veggirGroup.getChildren()) {
+            Rectangle veggur = (Rectangle) barn;
+            double minX = veggur.getLayoutX();
+            double maxX = minX + veggur.getWidth();
+            double minY = veggur.getLayoutY();
+            double maxY = minY + veggur.getHeight();
+
+            if (x >= minX && x < maxX && y >= minY && y < maxY) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void safnaPellet() {
+        Iterator<Node> iterator = pelletGroup.getChildren().iterator();
+        while (iterator.hasNext()) {
+            Node pellet = iterator.next();
+            if (Collectables.erAPellet(fxPacman, pellet)) {
+                iterator.remove();
+                gefaStig(100);
+            }
+        }
+    }
+
+    public void safnaCherry() {
+        Iterator<Node> iterator = cherryGroup.getChildren().iterator();
+        while (iterator.hasNext()) {
+            Node cherry = iterator.next();
+            if (Collectables.erACherry(fxPacman, cherry)) {
+                iterator.remove();
+                gefaStig(500);
+            }
+        }
+    }
+
+    public void buaTilPellets(Maze fxMaze) {
+        for (int x = 0; x <= 800; x += 25) {
+            for (int y = 0; y <= 800; y += 25) {
+                Iterator<Node> veggirIterator = veggirGroup.getChildren().iterator();
+                Iterator<Node> cherryIterator = cherryGroup.getChildren().iterator();
+                Pellet newPellet = new Pellet();
+                newPellet.setLayoutX(x);
+                newPellet.setLayoutY(y);
+                pelletGroup.getChildren().add(newPellet);
+                while (veggirIterator.hasNext()) {
+                    Node veggir = veggirIterator.next();
+                    if (Collectables.erEkkiEinmana(newPellet, veggir)) {
+                        pelletGroup.getChildren().remove(newPellet);
+                    }
+                }
+                while (cherryIterator.hasNext()) {
+                    Node cherry = cherryIterator.next();
+                    if (Collectables.erEkkiEinmana(newPellet, cherry)) {
+                        pelletGroup.getChildren().remove(newPellet);
+                    }
+                }
+                if (Collectables.erAPellet(fxPacman,newPellet)) {
+                    pelletGroup.getChildren().remove(newPellet);
+                }
+            }
+        }
+    }
+
+    private void gefaStig(int gefinnStig) {
+        Stig.set(Stig.get() + gefinnStig);
+    }
+
+    public void minnkaTima() {
+        Timi.set(Timi.get() - 1);
+    }
+
+    public boolean erLeikLokid() {
+        return Timi.get() == 0;
+    }
+}
---
+++ Pacman/src/main/java/vidmot/pacman/PacmanController.java
@@ -0,0 +1,98 @@
+package vidmot.pacman;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.fxml.FXML;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.util.Duration;
+
+import java.util.HashMap;
+
+public class PacmanController {
+    private final HashMap<KeyCode, Stefna> map= new HashMap<KeyCode, Stefna>();
+
+    @FXML
+    private Maze fxMaze;
+
+    public void initialize(){
+
+    }
+
+    /**
+     * Setur upp örvatakka og eventfilter þegar ýtt er á örvatakka sem breytir þá stefnunni.
+     */
+    public void orvatakkar() {
+        map.put(KeyCode.UP, Stefna.UPP);
+        map.put(KeyCode.DOWN, Stefna.NIDUR);
+        map.put(KeyCode.RIGHT, Stefna.HAEGRI);
+        map.put(KeyCode.LEFT, Stefna.VINSTRI);
+        fxMaze.getScene().addEventFilter(KeyEvent.ANY, this::stefna);
+    }
+
+    /**
+     * Breytir stefnunni á leikborðinu (sem kallar svo á pacman) þannig að pacman breytir um stefnu.
+     * Stefna fengin úr map í orvatakkar fallinu.
+     */
+    private void stefna(KeyEvent event){
+        if(map.get(event.getCode()) != null && fxMaze.erLoglegStefna(map.get(event.getCode()))){
+            fxMaze.setStefna(map.get(event.getCode()));
+        }
+        if(fxMaze.erAVegg()){
+            switch (fxMaze.getFyrriPacmanStefna()) {
+                case Stefna.UPP:
+                    if(map.get(event.getCode()) != Stefna.UPP){
+                        fxMaze.setPacmanLayoutY(fxMaze.getPacmanLayoutY(), +5);
+                    }
+                    break;
+                case Stefna.NIDUR:
+                    if(map.get(event.getCode()) != Stefna.NIDUR){
+                        fxMaze.setPacmanLayoutY(fxMaze.getPacmanLayoutY(), -5);
+                    }
+                    break;
+                case Stefna.HAEGRI:
+                    if(map.get(event.getCode()) != Stefna.HAEGRI){
+                        fxMaze.setPacmanLayoutX(fxMaze.getPacmanLayoutX(), -5);
+                    }
+                    break;
+                case Stefna.VINSTRI:
+                    if(map.get(event.getCode()) != Stefna.VINSTRI){
+                        fxMaze.setPacmanLayoutX(fxMaze.getPacmanLayoutX(), +5);
+                    }
+                    break;
+            }
+        }
+    }
+
+
+    /**
+     * Hefur loop leiksins sem lætur pacamn hreyfast sjálfur. Kallar á 20ms fresti á afram aðferðina sem
+     * er skilgrein í maze þar sem kallað er á pacman. Timelineið hefur delay 3 sek til þess að gefa notanda
+     * smá breather áður en leikur hefst. Keyrir endalaust.
+     */
+    public void hefjaLeik() {
+        fxMaze.buaTilPellets(fxMaze);
+        KeyFrame k = new KeyFrame(Duration.millis(10),
+                e ->{
+                    fxMaze.afram();
+                    fxMaze.safnaPellet();
+                    fxMaze.safnaCherry();
+                });
+        KeyFrame j = new KeyFrame(Duration.seconds(1),
+                e ->{
+                    fxMaze.minnkaTima();
+                    System.out.println("Time lowered");
+                    if (fxMaze.erLeikLokid()) {
+                        System.out.println("Done");
+                    }
+                });
+        Timeline t = new Timeline(k);
+        Timeline y = new Timeline(j);
+        t.setDelay(Duration.seconds(3));
+        t.setCycleCount(Timeline.INDEFINITE);
+        t.play();
+        y.setDelay(Duration.seconds(3));
+        y.setCycleCount(Timeline.INDEFINITE);
+        y.play();
+    }
+}
